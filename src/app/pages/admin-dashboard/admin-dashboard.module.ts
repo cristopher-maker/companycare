a87246500e0ecb67fd76cad163a901df66c6d1ed
adeus7054-afk <adeus7054@gmail.com>
2026-05-05 14:27:55 -0400

    Fix charts module for Angular 18 build

--- src/app/pages/admin-dashboard/admin-dashboard.module.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.module.ts
@@ -4,10 +4,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard.component';
-import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 @NgModule({
   declarations: [
